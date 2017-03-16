a463ae7e6bf5f9c8d6159f0942a3197cb435d41d
saibot777 <stefantrajkovic777@gmail.com>
2017-03-17 00:53:29 +0100

    psMenu Item UI done

--- ext-modules/psMenu/psMenuItemDirective.js
+++ ext-modules/psMenu/psMenuItemDirective.js
@@ -7,7 +7,8 @@ angular.module('psMenu').directive('psMenuItem', function() {
     return {
         require: '^psMenu',
         scope: {
-            label: '@'
+            label: '@',
+            icon: '@'
         },
         templateUrl: 'ext-modules/psMenu/psMenuItemTemplate.html',
         link: function(scope, el, attr, ctrl) {
