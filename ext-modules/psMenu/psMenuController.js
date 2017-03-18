e177b629b5b6232c20d848518aaf02f478e3df02
saibot777 <stefantrajkovic777@gmail.com>
2017-03-18 13:15:29 +0100

    routing implemented

--- ext-modules/psMenu/psMenuController.js
+++ ext-modules/psMenu/psMenuController.js
@@ -2,8 +2,15 @@
  * Created by stefan.trajkovic on 17.3.2017..
  */
 angular.module("psMenu").controller("psMenuController",
-    ['$scope',
-        function ($scope) {
+    ['$scope', '$rootScope',
+        function ($scope, $rootScope) {
+            this.setActiveElement = function (el) {
+                $scope.activeElement = el;
+            };
 
+            this.setRoute = function (route) {
+                $rootScope.$broadcast('ps-menu-item-selected-event',
+                    {route: route});
+            };
         }
     ]);
